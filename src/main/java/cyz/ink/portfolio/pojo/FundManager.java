b4826d0d744c112f7463262c561aa55af2b86ee9
cyzuestc <cyzuestc@users.noreply.github.com>
2019-08-09 13:24:07 +0800

    Add files via upload

---
+++ src/main/java/cyz/ink/portfolio/pojo/FundManager.java
@@ -0,0 +1,9 @@
+package cyz.ink.portfolio.pojo;
+
+public class FundManager {
+    private int id;
+    private String name;
+    private String password;
+    private String phone;
+    private int money;
+}
