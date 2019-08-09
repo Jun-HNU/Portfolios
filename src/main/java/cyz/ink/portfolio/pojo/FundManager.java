36b74a6778c0d8f1890949648a4d92711f0ef6e3
cyzink <421248827@qq.com>
2019-08-09 13:09:51 +0800

    first commit

--- src/main/java/cyz/ink/portfolio/pojo/FundManager.java
+++
@@ -1,9 +0,0 @@
-package cyz.ink.portfolio.pojo;
-
-public class FundManager {
-    private int id;
-    private String name;
-    private String password;
-    private String phone;
-    private int money;
-}
